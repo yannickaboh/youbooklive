529c74f1572af3d6cbaeb85ad244a78f885121cd
yannickaboh <yannickabohthierry@gmail.com>
2020-05-10 21:17:45 +0100

    set

--- youbooklive/settings.py
+++ youbooklive/settings.py
@@ -144,4 +144,4 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'travelapp/static'),)
